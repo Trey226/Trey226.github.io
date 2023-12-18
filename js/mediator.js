dd6119395704cc53b3c85ab71690bb65666fb244
Trey226 <bgilliam6@uco.edu>
2023-12-18 11:07:47 -0600

    second commit

--- js/mediator.js
+++ js/mediator.js
@@ -1,9 +1,15 @@
-var Mediator = (function() {
-    function loadPage(page) {
-        window.location.href = page;
-    }
+// Simple JavaScript to control slideshow - Add your own logic
+let currentSlide = 0;
+const slides = ['image1.jpg', 'image2.jpg', 'image3.jpg']; // Replace with actual image paths
 
-    return {
-        loadPage: loadPage
-    };
-})();
+function showSlide(index) {
+    const slideContainer = document.querySelector('.slideshow-container');
+    slideContainer.style.backgroundImage = `url(${slides[index]})`;
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000); // Change slide every 3 seconds
